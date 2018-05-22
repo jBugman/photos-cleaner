0fffe92281fae24d2d152efab875a157865cf270
Sergey Parshukov <codedby@bugman.me>
2018-05-23 01:26:40 +0200

    Standalone web-based auth

---
+++ auth/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import './App.css'
+import { GoogleLogin } from 'react-google-login'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+class App extends Component {
+  state = {
+    clientId: null,
+    code: null
+  }
+
+  onIdChange = (id) => this.setState({ clientId: id })
+
+  googleFailure = (e) => console.log(e)
+
+  googleSuccess = (e) => this.setState({ code: e.code })
+
+  render () {
+    return (
+      <div className='App'>
+        <ClientId onIdChange={this.onIdChange} />
+        {this.state.clientId &&
+          <GoogleLogin clientId={this.state.clientId}
+            buttonText='Authorize'
+            onSuccess={this.googleSuccess}
+            onFailure={this.googleFailure}
+            responseType='code'
+          />
+        }
+        {this.state.code &&
+          <div>
+            <span>
+              Code: {this.state.code}
+            </span>
+            <CopyToClipboard text={this.state.code}>
+              <button>Copy</button>
+            </CopyToClipboard>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+class ClientId extends Component {
+  state = {
+    value: ''
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onIdChange(this.state.value)
+  }
+
+  onChange = (e) => this.setState({ value: e.target.value })
+
+  render = () =>
+    <form onSubmit={this.onSubmit}>
+      <input type='text' value={this.state.value} onChange={this.onChange} />
+      <button type='submit'>
+        Create the button
+      </button>
+    </form>
+}
+
+export default App
