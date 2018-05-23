565f433c34d9e0c17e9349787ef315670b4a6efc
Sergey Parshukov <codedby@bugman.me>
2018-05-23 02:09:40 +0200

    Use react version as rust's static assets

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 extern crate actix_web;
 extern crate env_logger;
 
+use std::path::Path;
+
 use actix_web::fs::{NamedFile, StaticFiles};
 use actix_web::middleware::Logger;
 use actix_web::{server, App, HttpRequest, Result, http::Method};
 
 const BIND_ADDRESS: &str = "127.0.0.1:8080";
+const STATIC_FOLDER: &str = "auth/build";
 
 fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    let app = || {
+    let static_path = Path::new(STATIC_FOLDER).join("static");
+    let app = move || {
         App::new()
             .middleware(Logger::default())
             .resource("/", |r| r.method(Method::GET).f(index))
-            .handler("/static", StaticFiles::new("resources"))
+            .handler("/static", StaticFiles::new(&static_path))
     };
     server::new(app)
         .bind(BIND_ADDRESS)
@@ -24,5 +28,6 @@ fn main() {
 }
 
 fn index(_: HttpRequest) -> Result<NamedFile> {
-    Ok(NamedFile::open("resources/index.html")?)
+    let path = Path::new(STATIC_FOLDER).join("index.html");
+    Ok(NamedFile::open(path)?)
 }
