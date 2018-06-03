77956cd330e8f2e7c149b2e05d534a04f6eea74f
Sergey Parshukov <codedby@bugman.me>
2018-06-03 16:59:49 +0300

    Transition to yarn, fixed dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
-// TODO: convert to .jsx
-// eslint-disable-next-line react/jsx-filename-extension
 render(<App />, document.getElementById('root'))
 
 if (module.hot) {
